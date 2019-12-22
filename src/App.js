0d0c12e3da6855dfbee36c972ed7b6f3040dd15a
Anyelo <anyeloert@gmail.com>
2019-12-21 23:02:04 -0500

    actualizacion 1

--- src/App.js
+++ src/App.js
@@ -9,30 +9,30 @@ import AdminiCitas from './components/AdminiCitas.js'
 
 class App extends Component {
   state={
-    cita:[]
+    cita:[]//arreglo que almacena todas las citas creadas
   }
 
-  componentDidMount(){
-    const citasLS = localStorage.getItem('citas')
-    if (citasLS){
+  componentDidMount(){ //ciclo de vida que ejecuta el codigo despues del renderizado
+    const citasLS = localStorage.getItem('citas')//obtiene de local storage lo que esta almacenado con el nombre "citas"
+    if (citasLS){ //pregunta si la variable "citasLS" tiene almacenado algo, si lo tiene lo agrega al state
       this.setState({
-        cita:JSON.parse(citasLS)
+        cita:JSON.parse(citasLS) //local storage solo almacena datos de tipo string, por lo que se debe cambiar para poder ser usado
       })
     }
   }
-  componentDidUpdate(){
-    localStorage.setItem('citas', JSON.stringify(this.state.cita))
+  componentDidUpdate(){// ciclo de vida que ejecuta el codigo cuando ocurre algun cambio en el componente
+    localStorage.setItem('citas', JSON.stringify(this.state.cita))// toma lo que esta en el state, lo convierte en string y lo guarda en el local storage
   }
-  crearNuevaCita = datos =>{
+  crearNuevaCita = datos =>{//funcion que añade las nuevas citas al state de app.js
     this.setState({
-      cita:[...this.state.cita, datos]
+      cita:[...this.state.cita, datos] //al state agrega lo que ya estaba en state y la nueva cita creada
     })
   }
-  eliminarCita = id =>{
-    let copiaState = [...this.state.cita]
-    const citasNoEliminadas = copiaState.filter(cita => cita.id!==id)
+  eliminarCita = id =>{// funcion que permite eliminar una cita del state con el id suministrado
+    let copiaState = [...this.state.cita]// una copia del actual state
+    const citasNoEliminadas = copiaState.filter(cita => cita.id!==id)// en esta variable se almacenan todas las citas cuyo id sea distinto al de entrada, 
     this.setState({
-      cita:[...citasNoEliminadas]
+      cita:[...citasNoEliminadas]// se almacena el array creado por la funcion filter en el state
     })
   }
   render() {
